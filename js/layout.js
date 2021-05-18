c812eda9ad9f0f129a853c561f2077600c3a3a72
tikuwadayo <75162565+tikuwadayo@users.noreply.github.com>
2021-05-18 22:29:29 +0900

    Update layout.js

--- js/layout.js
+++ js/layout.js
@@ -13,15 +13,23 @@ var Layout = function () {
 
 
     
-$(function(){
-   $(window).on('load',function(){
-     $("#page-loading").delay(1000).fadeOut('slow');
-   });
-   function loaderClose(){
-     $("#page-loading").fadeOut('slow');
-   }
-   setTimeout(loaderClose,10000);
-});
+var bg = document.getElementById('loader-bg'),
+  loader = document.getElementById('loader');
+/* ロード画面の非表示を解除 */
+bg.classList.remove('is-hide');
+loader.classList.remove('is-hide');
+
+/* 読み込み完了 */
+window.addEventListener('load', stopload);
+
+/* 10秒経ったら強制的にロード画面を非表示にする */
+setTimeout('stopload()', 10000);
+
+/* ロード画面を非表示にする処理 */
+function stopload() {
+  bg.classList.add('fadeout-bg');
+  loader.classList.add('fadeout-loader');
+}
     
     
     // handle on page scroll
